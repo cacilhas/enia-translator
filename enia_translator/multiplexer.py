c7f2dd39c84095badfac82eeafe41ba1e5462f14
Arĥimedeς ℳontegasppα ℭacilhας <batalema@cacilhas.info>
2018-05-05 23:13:50 -0300

    more semantic fix

--- enia_translator/multiplexer.py
+++ enia_translator/multiplexer.py
@@ -16,14 +16,14 @@ def distribute(f: Callable[[T1], T2],
     tasks = []
     for arg in args:
         future = asyncio.Future(loop=loop)
-        future.add_done_callback(partial(call_result, callback))
+        future.add_done_callback(partial(pass_result, callback))
         tasks.append(coro(partial(f, arg), future))
     loop.run_until_complete(
         asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED, loop=loop)
     )
 
 
-def call_result(f: Callable[[Any], None], future: asyncio.Future) -> None:
+def pass_result(f: Callable[[Any], None], future: asyncio.Future) -> None:
     f(future.result())
 
 
