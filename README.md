139117a711f704bf32016682ffc0129e31864eab
Arĥimedeς ℳontegasppα ℭacilhας <batalema@cacilhas.info>
2018-05-06 00:19:32 -0300

    support multiple words search

--- README.md
+++ README.md
@@ -29,9 +29,6 @@ The source for matches is the
 - Support other languages with no config-file editing (using
   [`argparse`](https://docs.python.org/3/library/argparse.html)).
 - Support inexact search.
-- Search for multiple words.
-- Use [AsyncIO](https://docs.python.org/3/library/asyncio.html) for
-  parallelizing searches and processing.
 
 ## Copyrights
 
--- enia_translator/_scripts/en_ia.py
+++ enia_translator/_scripts/en_ia.py
@@ -1,12 +1,10 @@
 from argparse import ArgumentParser
-from enia_translator import EniaWordSearcher
+from enia_translator.multisearch import search
 from enia_translator.settings import load_settings
 
 
 def entrypoint() -> None:
     parser = ArgumentParser(description='translate en -> ia')
-    parser.add_argument('word', type=str, help='word to search')
+    parser.add_argument('words', type=str, nargs='+', help='word to search')
     args = parser.parse_args()
-    searcher = EniaWordSearcher(args.word, load_settings())
-    for result in searcher.search(args.word):
-        print(result)
+    search(' '.join(args.words), print)
