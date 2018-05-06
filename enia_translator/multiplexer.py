fb91aff1d69ebaa5583d43fc35bc37c03d15a5c7
Arĥimedeς ℳontegasppα ℭacilhας <batalema@cacilhas.info>
2018-05-05 23:12:03 -0300

    use more semantic approach
    We don’t want to gather tasks, we want to wait for them.

--- enia_translator/multiplexer.py
+++ enia_translator/multiplexer.py
@@ -18,7 +18,9 @@ def distribute(f: Callable[[T1], T2],
         future = asyncio.Future(loop=loop)
         future.add_done_callback(partial(call_result, callback))
         tasks.append(coro(partial(f, arg), future))
-    loop.run_until_complete(asyncio.gather(*tasks, loop=loop))
+    loop.run_until_complete(
+        asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED, loop=loop)
+    )
 
 
 def call_result(f: Callable[[Any], None], future: asyncio.Future) -> None:
