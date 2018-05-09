b5f74cad5f3fe6ed42ebd2e6dff6bb627a3d41d3
Arĥimedeς ℳontegasppα ℭacilhας <batalema@cacilhas.info>
2018-05-08 23:28:47 -0300

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('./README.md') as fp:
 
 setup(
     name='enia-translator',
-    version='1.2.2',
+    version='1.2.3',
     provides=['enia_translator'],
     description='Search for Interligua translations on-line',
     long_description=long_description,
